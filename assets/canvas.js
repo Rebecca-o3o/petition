c361557f4053d1d9ad4ce1992b036614112262aa
beccyo3o <rebecca.krummel@gmail.com>
2017-08-17 12:00:59 +0200

    add draw functionality to canvas

---
+++ assets/canvas.js
@@ -0,0 +1,41 @@
+var pos = { x: 0, y: 0 };
+var mouseDownStatus = false;
+
+var setPosition = function (e) {
+    pos.x = e.offsetX;
+    pos.y = e.offsetY;
+};
+
+var draw = function(e){
+    // reference to html:
+    var context = $('#canv')[0].getContext('2d');
+
+    //prepare strokes:
+    context.strokeStyle = 'black';
+    context.lineWidth = '3';
+    context.lineCap = 'round';
+
+    //begin:
+    context.beginPath();
+
+    context.moveTo(pos.x, pos.y);           // from
+    setPosition(e);                         // check new position
+    context.lineTo(pos.x, pos.y);           // to
+    context.stroke();                       // draw it!
+};
+
+//log mouse positions on mousedown:
+$('#canv').mousedown(setPosition, function(e){
+    mouseDownStatus = true;
+    setPosition(e);
+});
+
+$('#canv').mousemove(setPosition, function(e){
+    if (mouseDownStatus) {
+        draw(e);
+    }
+});
+
+$('#canv').mouseup(function(){
+    mouseDownStatus = false;
+});
