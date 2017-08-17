fa5f8c14560092f6ecb98b269bfb76d56d9ba6d7
beccyo3o <rebecca.krummel@gmail.com>
2017-08-17 16:54:02 +0200

    add body-parser

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 //npm modules:
+const bodyParser = require('body-parser');
 const express = require("express");
 var app = express();
 var hb = require('express-handlebars');
@@ -10,12 +11,21 @@ app.set('view engine', 'handlebars');
 //middleware for static assets
 app.use(express.static('assets'));
 
+//middleware to use request bodies containing values from user inputs
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
 //routes
 app.get("/", function (req, res){
     res.render("home", {
         layout: "main"
     });
 });
+
+// app.post("/", function (req, res){
+//     console.log()
+// });
 
 app.get("/thanks", function (req, res){
     res.render("thanks", {
